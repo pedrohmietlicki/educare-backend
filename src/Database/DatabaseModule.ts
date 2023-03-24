a794abd35eb03f3636218842812165a359cd1a2e
Pedro H. S. Mietlicki <pedromietlicke@gmail.com>
2023-03-24 20:45:20 -0300

    feat: simplify database conection

--- src/Database/DatabaseModule.ts
+++
@@ -1,5 +0,0 @@
-import { DynamicModule } from "@nestjs/common";
-
-export interface DatabaseModule {
-    connect: (url: string, options: any | null) => DynamicModule
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseFactory } from './Database/DatabaseFactory';
-import { MongoModule } from './Database/MongoModule';
 
 @Module({
-  imports: [DatabaseFactory.createDatabase(new MongoModule, 'mongodb://root:root@localhost:27017')],
+  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017')],
   controllers: [AppController],
   providers: [AppService],
 })
