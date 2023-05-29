8b53dae55eab4633d2142e18f4a2e86a088a0145
Pedro H. S. Mietlicki <pedromietlicke@gmail.com>
2023-05-29 15:11:09 -0300

    refactor: change how import UserRepository

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,8 @@
 import { User } from 'src/entities/user';
-import { UserRepository } from 'src/repositories/user.repository';
+import UserRepository from 'src/repositories/user.repository';
 import { UserNotFound } from '../errors/user.error';
 export default class UserService {
   constructor(private userRepository: UserRepository) {}
-
-  async getAll(): Promise<User[]> {
-    return await this.userRepository.getUsers();
-  }
 
   async getById(id: string): Promise<User> {
     const user = await this.userRepository.getUserById(id);
