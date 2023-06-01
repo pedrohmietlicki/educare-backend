03cebe412fb371a74de0262607d1429f8bb63d74
Pedro H. S. Mietlicki <pedromietlicke@gmail.com>
2023-06-01 15:25:17 -0300

    feat: change errror in http exception

--- src/errors/user.error.ts
+++ src/errors/user.error.ts
@@ -1,12 +1,14 @@
-export class UserException extends Error {}
+import { HttpException } from '@nestjs/common';
+
+export class UserException extends HttpException {}
 
 export class UserExists extends UserException {
   constructor() {
-    super('Já existe um usuário com esse email');
+    super('Já existe um usuário com esse email', 400);
   }
 }
 export class UserNotFound extends UserException {
   constructor() {
-    super('Usuário não encontrado');
+    super('Usuário não encontrado', 404);
   }
 }
