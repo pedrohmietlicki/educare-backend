d33a804814cbd5e0cee6b3dea21c46cf62a4fa43
Pedro H. S. Mietlicki <pedromietlicke@gmail.com>
2024-01-15 14:08:45 -0300

    change variable name

--- src/usecases/create-user.ts
+++ src/usecases/create-user.ts
@@ -2,16 +2,17 @@ import { UserCreate } from 'src/models/user.model';
 import UserRepository from 'src/repositories/user.repository';
 import * as bcrypt from 'bcrypt';
 import { UserExists } from '../errors/user.error';
+const SALT_ROUNDS = 10;
 export default class CreateUser {
   constructor(private userRepository: UserRepository) {}
   async perform({ email, password, name }: UserCreate) {
     if (await this.checkIfExists(email)) throw new UserExists();
 
-    const hashPassword = await bcrypt.hash(password, 10);
+    const criptedPassword = await bcrypt.hash(password, SALT_ROUNDS);
     return await this.userRepository.createUser({
       email,
       name,
-      password: hashPassword,
+      password: criptedPassword,
     });
   }
   private async checkIfExists(email: string): Promise<boolean> {
